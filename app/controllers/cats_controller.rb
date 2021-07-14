80f53edd6179b6fca795678eb689d7c7f90fa50b
CatGitter <utautaiiinko@i.softbank.jp>
2021-07-14 16:13:15 +0900

    投稿編集機能実装

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,5 +1,8 @@
 class CatsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :in_cat, only: [:show, :edit, :update]
+  before_action :move_index, only: [:edit, :update]
+  
 
   def index
     @cats = Cat.all.order("created_at DESC")
@@ -19,12 +22,32 @@ class CatsController < ApplicationController
   end
 
   def show
-    @cat = Cat.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @cat.update(cat_params)
+      redirect_to cat_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def cat_params
     params.require(:cat).permit(:image, :name, :age, :breed, :prefecture_id, :sex_id, :vaccine_id, :castration_id, :personality, :health, :adopt_method, :remarks).merge(user_id: current_user.id)
+  end
+
+  def in_cat
+    @cat = Cat.find(params[:id])
+  end
+
+  def move_index
+    unless current_user.id = @cat.user_id
+      redirect_to action: :index
+    end
   end
 end
