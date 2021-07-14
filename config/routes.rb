90a65842d98fc908ebe345a8df100c41719b484a
CatGitter <utautaiiinko@i.softbank.jp>
2021-07-14 17:14:29 +0900

    削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'cats/index'
   root to: "cats#index"
-  resources :cats, only: [:new, :create, :show, :edit, :update]
+  resources :cats, only: [:new, :create, :show, :edit, :update, :destroy]
 end
