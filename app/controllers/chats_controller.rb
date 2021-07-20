239f0b5c9421705a67ec7ed4f9eac66d24944d1b
CatGitter <utautaiiinko@i.softbank.jp>
2021-07-20 15:17:11 +0900

    Ajaxを用いた非同期通信の基礎記述（未実装）

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -19,7 +19,7 @@ class ChatsController < ApplicationController
   def create
     @chat = current_user.chats.new(chat_params)
     @chat.save
-    redirect_to request.referer
+    render json:{ post: @chat }
   end
 
   private
--- app/javascript/chat.js
+++ app/javascript/chat.js
@@ -1,3 +1,13 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <tr>
+      <td>${item.user.nickname}</td>
+      <td>${item.content}</td>
+    </tr>`;
+  return html;
+};
+
 function post (){
   const submit = document.getElementById("submit");
   submit.addEventListener("click", (e) => {
@@ -8,6 +18,16 @@ function post (){
     XHR.open("POST", "/chats", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 };
 
